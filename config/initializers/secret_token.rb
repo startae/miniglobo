8c1373218aa7b3ec764c48d8cf9b716f1e2c2757
Julio Protzek <julioprotzek@gmail.com>
2013-06-19 14:50:27 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miniglobo::Application.config.secret_key_base = 'f356092bda658458ad5c3224997d7611d60c1fc20cd750b1e5338d35375a503f4c956510428ac1c6ca2158ad1813514100a6b340899ee2791c861d4e4abcc2e8'
