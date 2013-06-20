ac949727f918e04fcee8bd68a8ca2be3070c214d
Julio Protzek <julioprotzek@gmail.com>
2013-06-20 20:11:35 -0300

    related articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   belongs_to :category
   mount_uploader :image, ImageUploader
   validates :title, :summary, :image, :category, presence: true
+
+  def related
+    category.articles.where('articles.id != ?', id).limit(3)
+  end
 end
