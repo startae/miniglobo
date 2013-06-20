891a89beefdca491bca5ace660856079897a921d
Julio Protzek <julioprotzek@gmail.com>
2013-06-20 19:51:26 -0300

    homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,5 @@
 class HomepageController < ApplicationController
   def index
+    @categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Miniglobo::Application.routes.draw do
-  namespace :admin, path_names: {new: 'criar', edit: 'editar'} do
+  namespace :admin, path_names: { new: 'criar', edit: 'editar' } do
     get '' => 'articles#index'
     resources :articles, path: 'noticias'
     resources :categories, path: 'categorias'
@@ -7,5 +7,6 @@ Miniglobo::Application.routes.draw do
 
   get 'frontend' => 'frontend#index'
   get 'frontend/:template' => 'frontend#show'
+
   root 'homepage#index'
 end
